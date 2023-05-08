3446491ce744a81671dd87bdb373d92df61994e8
Alex <alexbr7@hotmail.es>
2023-05-08 15:40:05 +0200

    array asociativo con meses y desafios

--- challenges/views.py
+++ challenges/views.py
@@ -1,36 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseNotFound 
+from django.http import HttpResponse,HttpResponseNotFound
 
 # Create your views here.
 
+monthly_challenges = {
+        "january": "Aprendemos Django",
+        "february": "Aprendemos Aleman",
+        "march": "Empezamos las practicas con ganas",
+        "april": "Empezamos a entrenar",
+        "may": "Dieta saludable",
+        "june": "Leer al menos 50 paginas al dia",
+        "july": "Empezar a correr",
+        "august": "Ya no se me ocurren mas ideas",
+        "september": "Aprender a cocinar",
+        "october": "Aprender a tocar la guitarra",
+        "november": "Aprender a bailar",
+        "december": "Aprender a dibujar"
+    }
 def monthly_challenge_by_number(request, month):
-    respuesta = None
-    if(month == 1):
-        respuesta = "enero"
-    elif(month == 2):
-        respuesta = "febrero"
-    else:
-        respuesta = "Este mes no esta disponible"
-    return HttpResponse(respuesta)
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    return HttpResponse(monthly_challenges[months[month-1]])
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = "Aprendemos Django "
-    elif month == 'february':
-        challenge_text = "Aprendemos Aleman"
-    elif month == 'march':
-        challenge_text = "Empezamos las practicas con ganas"
-    elif month == 'april':
-        challenge_text = "Empezamos a entrenar"
-    elif month == 'may':
-        challenge_text = "Dieta saludable"
-    elif month == 'june':
-        challenge_text = "Leer al menos 50 paginas al dia"
-    elif month == 'july':
-        challenge_text = "Empezar a correr"
-    elif month == 'august':
-        challenge_text = "Ya no se me ocurren mas ideas"
-    else:
-        return HttpResponseNotFound("Este mes no esta disponible")
+    challenge_text = monthly_challenges[month]
     return HttpResponse(challenge_text)
